db8c32eea355431d918b0bcd955e4df1b81d31b1
unknown <bwcastillo@uc.cl>
2021-04-26 18:49:22 -0400

    Avancé en el flowork, resulta problemas en caracteres de nombre columnas de datos a importar

--- 1_openData.R
+++ 1_openData.R
@@ -2,10 +2,33 @@ library(sf)
 library(RPostgres)
 library(tidyverse)
 
-censo1992<-colnames(readRDS("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds"))
-censo2002<-colnames(readRDS("C:/CEDEUS/Censos/Censo2002_Persona_Full.Rds"))
-censo2012<-colnames(readRDS("C:/CEDEUS/Censos/Censo2012_Persona_Full.Rds"))
-View(test)
+
+# 0. Leyendo los datos y creando summaries: Tipo de datos y largo ------------
+
+# Bonus track:Forma hechiza de guardar datos de RDS a csv
+write.csv(readRDS("C:/CEDEUS/Censos/Censo2002_Persona_Full.Rds"),"C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.csv",na="0")
+write.csv(readRDS("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds"),"C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo1992.csv",na="0")
+write.csv(readRDS("C:/CEDEUS/Censos/Censo2012_Persona_Full.Rds"),"C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2012.csv",na="0")
+write.csv(readRDS("C:/CEDEUS/Censos/Censo2017_Persona_Full.Rds"),"C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2017.csv",na="0")
+
+
+write.csv(as.data.frame(test), "p35.csv", na="0")
+# 0.0 sabiendo como se debería llamar las columnas de la tabla
+
+# Formato RDS:
+#censo1992<-colnames(readRDS("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds"))
+#censo2002<-colnames(readRDS("C:/CEDEUS/Censos/Censo2002_Persona_Full.Rds"))
+#censo2012<-colnames(readRDS("C:/CEDEUS/Censos/Censo2012_Persona_Full.Rds"))
+
+# Formato csv:
+#head(read.csv("C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.csv"))
+
+# Formato shape:
+#colnames(st_read("C:/CEDEUS/2021/abril1_ciudadesCosteras/input/Cartografia censo 2012-20210402T161853Z-001/Cartografia censo 2012/Carto_Region_5.gdb",layer="MANZANA"))
+
+
+# 0.1 Viendo el largo de todos los caracteres
+#Ref:grasshoppermouse: https://www.reddit.com/r/rstats/comments/azumgy/is_there_a_summarylike_function_which_gives_nchar/
 
 test2012<-summarise_all(readRDS("C:/CEDEUS/Censos/Censo2012_Persona_Full.Rds"),funs(
   case_when(is.character(.)~max(nchar(.), na.rm=T),
@@ -20,13 +43,10 @@ test1992<-summarise_all(readRDS("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds"),f
             is.numeric(.) ~max(nchar(as.character(.)), na.rm=T))))
 
 
-(c(1238,43,56,5,76))
-#write.csv(readRDS("C:/CEDEUS/Censos/Censo2002_Persona_Full.Rds"),"C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.csv")
-#head(read.csv("C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.csv"))
 
-colnames(st_read("C:/CEDEUS/2021/abril1_ciudadesCosteras/input/Cartografia censo 2012-20210402T161853Z-001/Cartografia censo 2012/Carto_Region_5.gdb",layer="MANZANA"))
-
+# 0.2 Summaries de tipo de dato
 #https://stackoverflow.com/questions/22930292/is-it-possible-to-store-str-output-in-a-r-object
+
 test<-spread(group_by(as.data.frame(summary.default(readRDS("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds")), by=Var1)),key = Var2, value = Freq)
 
 abreColumnas<-function(x){tidyr::spread(dplyr::group_by(as.data.frame(summary.default(readRDS(x),funs(summarise_all(max(nchar(.))))), by=Var1)),key = Var2, value = Freq)}
@@ -34,8 +54,6 @@ abreColumnas<-function(x){tidyr::spread(dplyr::group_by(as.data.frame(summary.de
 censo1992<-abreColumnas("C:/CEDEUS/Censos/Censo1992_Persona_Full.Rds")
 censo2002<-abreColumnas("C:/CEDEUS/Censos/Censo2002_Persona_Full.Rds")
 censo2012<-abreColumnas("C:/CEDEUS/Censos/Censo2012_Persona_Full.Rds")
-
-
 
 # 1. Aprendiendo a crear y  moverme entre BBDD ----------------------------
 
@@ -75,7 +93,24 @@ dbSendQuery(conn, "DROP DATABASE test")
 dbDisconnect(conn)
 
 
-# Creando esquemas y tablas -----------------------------------------------
+# 2. Creando esquemas y tablas -----------------------------------------------
+
+# 2.1. Creando esquema y tabla a partir de los summaries
+
+tablita<-function(x,y){
+  a<-cbind(variables=row.names(as.data.frame(t(x))),as.data.frame(t(x))) 
+
+  rownames(a)<- 1:nrow(a)
+
+  a<-left_join(y, a, by=c("Var1"="variables"))
+  a<-a[,c(1,4,5)]
+  colnames(a)<-c("variable","clase","largo")
+  return(a)
+}
+
+censo_1992<-tablita(test1992, censo1992)
+censo_2012<-tablita(test2012, censo2012)
+censo_2002<-tablita(test2002, censo2002)
 
 #me vuelvo a conectar a la bbdd
 
@@ -87,7 +122,7 @@ dbSendQuery(conn, "set search_path to censos;")
 #paste(test$Var1, test$Mode)
 
 test<-paste(c(paste(dQuote("ID"), "SERIAL PRIMARY KEY"),
-    paste(paste(test$Var1, test$Mode), "NOT NULL")),
+    paste(paste(test$variable, test$clase), "(",test$largo,")", "NOT NULL")),
     collapse=",")
 
 dbSendQuery(conn,paste0("CREATE TABLE censo1992 (",test, ")")) #Funciona!
@@ -100,9 +135,9 @@ gsub("\\.","",censo2002$Var1)
 creaTabla<-function(x){
   y<-deparse(substitute(x))
   
-  x$Var1<-gsub("\\.","",x$Var1)
+  x$variable<-gsub("\\.","",x$variable)
   x<-paste(c(paste("ID", "SERIAL PRIMARY KEY"),
-                                  paste(paste(x$Var1, x$Mode), "NOT NULL")),
+                                  paste(paste(x$variable, x$clase), "(",x$largo,")", "NOT NULL")),
                                 collapse=",")
   
                      x<-paste0("CREATE TABLE ",y," (",x, ")")
@@ -112,14 +147,21 @@ creaTabla<-function(x){
 
 deparse(substitute(test))
 class(deparse(quote(test)))
-test<-creaTabla(censo2002)
+creaTabla(test)
 
-dbSendQuery(conn,creaTabla(censo2002))
+dbSendQuery(conn,creaTabla(test))
+dbSendQuery(conn,creaTabla(censo_1992))
+dbSendQuery(conn,creaTabla(censo_2002))
+dbSendQuery(conn,creaTabla(censo_2012))
 
-dbSendQuery(conn, "\copy censos("censo2002") C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.7z' delimiter ';' csv header;")
+
+paste(c("(id",as.vector(censo_1992$variable),")"),  collapse=",")
+
+(censo_1992$variable)
+#dbSendQuery(conn, "\copy censos('test') C:/CEDEUS/2021/abril1_ciudadesCosteras/input/censo2002.7z' delimiter ';' csv header;")
 View(test)
 
-C:/CEDEUS/2021/abril1_ciudadesCosteras/input
+#C:/CEDEUS/2021/abril1_ciudadesCosteras/input#
 
-paste(c("(ID",as.vector(censo2002$Var1),")"), collapse=",")
+#paste(c("(ID",as.vector(censo2002$Var1),")"),  collapse=",")
 
